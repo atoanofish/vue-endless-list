9d99afaefa8eecbc73b61c81f1bd033de1e61288
atoanofish <276026712@qq.com>
2018-02-27 16:25:36 +0800

    test

--- src/components/EndlessTree/Node.vue
+++ src/components/EndlessTree/Node.vue
@@ -20,82 +20,82 @@
 </template>
 
 <script>
-    export default {
-        name: 'node',
-        data() {
-            return {};
-        },
-        props: {
-            node: {
-                type: [Number, String, Object, Array],
-                required: true
-            },
-            checked: {
-                type: Boolean,
-                default: false
-            },
-            checkHandle: {
-                type: [Function],
-                required: true
-            },
-            nodeClick: {
-                type: [Function],
-                required: true
-            }
-        },
-        methods: {
-            nodeExpand(model) {
-                this.nodeClick(model);
-            },
-            selectNode(e, node) {
-                this.checkHandle(e.target.checked, node);
-            },
-            isFolder(item) {
-                let folderClass = '';
-                if (!item[this.folderAttr]) {
-                    this.expand ? folderClass = 'folder-open' : folderClass = 'folder';
-                } else if (item[this.folderAttr] && this.iconInit) {
-                    folderClass = this.iconInit(item);
-                }
-                return folderClass;
-            }
-        }
-    };
+export default {
+  name: "node",
+  data() {
+    return {};
+  },
+  props: {
+    node: {
+      type: [Number, String, Object, Array],
+      required: true
+    },
+    checked: {
+      type: Boolean,
+      default: false
+    },
+    checkHandle: {
+      type: [Function],
+      required: true
+    },
+    nodeClick: {
+      type: [Function],
+      required: true
+    }
+  },
+  methods: {
+    nodeExpand(model) {
+      this.nodeClick(model);
+    },
+    selectNode(e, node) {
+      this.checkHandle(e.target.checked, node);
+    },
+    isFolder(item) {
+      let folderClass = "";
+      if (!item[this.folderAttr]) {
+        this.expand ? (folderClass = "folder-open") : (folderClass = "folder");
+      } else if (item[this.folderAttr] && this.iconInit) {
+        folderClass = this.iconInit(item);
+      }
+      return folderClass;
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-    p {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        color: #666666;
-        & > span {
-            /*display: inline-block;*/
-            vertical-align: middle;
-        }
-        .content {
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            /*width: 89%;*/
-        }
-        .expand-btn {
-            width: 20px;
-            height: 20px;
-            display: inline-block;
-            border: 1px solid #eee;
-            text-align: center;
-            line-height: 20px;
-        }
-        .folder {
-            position: relative;
-            top: 3px;
-            color: #979797;
-        }
-        .file {
-            position: relative;
-            top: 2px;
-            margin-left: 4px;
-        }
-    }
+p {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  color: #666666;
+  & > span {
+    /*display: inline-block;*/
+    vertical-align: middle;
+  }
+  .content {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    /*width: 89%;*/
+  }
+  .expand-btn {
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+    border: 1px solid #eee;
+    text-align: center;
+    line-height: 20px;
+  }
+  .folder {
+    position: relative;
+    top: 3px;
+    color: #979797;
+  }
+  .file {
+    position: relative;
+    top: 2px;
+    margin-left: 4px;
+  }
+}
 </style>
